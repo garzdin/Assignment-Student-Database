bf5635e54952bda14e56def82760f59efec9bc41
Teodor Garzdin <tsg@hugelawn.com>
2017-01-09 10:41:28 +0100

    Initial Commit

---
+++ Assignment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment
+//
+//  Created by Teodor on 09/01/2017.
+//  Copyright © 2017 TeodorGarzdin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
