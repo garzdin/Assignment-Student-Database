101c6ce81b723d3481fc858e8c85044f781a6102
Teodor Garzdin <tsg@hugelawn.com>
2017-01-09 11:19:17 +0100

    Import code.

--- Assignment/main.cpp
+++ Assignment/main.cpp
@@ -6,10 +6,215 @@
 //  Copyright © 2017 TeodorGarzdin. All rights reserved.
 //
 
+#include <string>
+#include <fstream>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Date {
+public:
+    unsigned int day;
+    unsigned int month;
+    unsigned int year;
+
+    Date(const Date& d) {
+        day = d.day;
+        month = d.month;
+        year = d.year;
+    }
+    
+    Date(const unsigned int d = 0, const unsigned int m = 0, const unsigned int y = 0) {
+        this->day = d;
+        this->month = m;
+        this->year = y;
+    }
+    
+    friend std::ostream& operator<<(std::ostream& stream, Date& date);
+    
+    int operator==(const Date& d) {
+        return day == d.day && month == d.month && year == d.year;
+    }
+    
+    Date& operator=(const Date& d) {
+        if(this != &d) {
+            day = d.day;
+            month = d.month;
+            year = d.year;
+        }
+        
+        return *this;
+    }
+    
+    unsigned int getDay() {
+        return day;
+    }
+    
+    void setDay(const unsigned int d) {
+        if (d > 0) {
+            day = d;
+        } else {
+            throw new std::invalid_argument("Day can't be negative");
+        }
+    }
+    
+    unsigned int getMonth() {
+        return month;
+    }
+    
+    void setMonth(const unsigned int m) {
+        if (m > 0) {
+            month= m;
+        } else {
+            throw new std::invalid_argument("Month can't be negative");
+        }
+    }
+    
+    unsigned int getYear() {
+        return year;
+    }
+    
+    void setYear(const unsigned int y) {
+        if (y > 0) {
+            year = y;
+        }
+        else {
+            throw new std::invalid_argument("Year can't be negative");
+        }
+    }
+    
+    ~Date () {
+        std::cout<<"Destructing <Date>"<<std::endl;
+    }
+};
+
+class Student {
+public:
+    std::string name;
+    std::string facNumber;
+    Date birthDate;
+    float avScore;
+    
+    Student() {}
+    
+    Student(std::string n, std::string fn, Date* d, float s) {
+        this->name = n;
+        this->facNumber = fn;
+        this->birthDate = *d;
+        this->avScore = s;
+    }
+    
+    ~Student() {
+        std::cout<<"Destructing <Student>"<<std::endl;
+    }
+    
+    friend std::ostream& operator<<(std::ostream& stream, Student& student);
+    
+    void setName(std::string n) { name = n; }
+    
+    std::string getName() { return name; }
+    
+    void setFacNumber(std::string id) { facNumber = id; }
+    
+    std::string getFacNumber() { return facNumber; }
+    
+    void setAvScore(float s) { avScore = s; }
+    
+    float getAvScore() { return avScore; }
+    
+    int showYears(Date date) {
+        if (date.getMonth() > this->birthDate.getMonth()) {
+            return (date.getYear() - this->birthDate.getYear());
+        } else {
+            if (date.getMonth() < this->birthDate.getMonth()) {
+                return (date.getYear() - this->birthDate.getYear() - 1);
+            } else {
+                if (date.getDay() > this->birthDate.getDay()) {
+                    return (date.getYear() - this->birthDate.getYear());
+                } else {
+                    return (date.getYear() - this->birthDate.getYear() - 1);
+                }
+            }
+        }
+    }
+};
+
+class Potok {
+public:
+    unsigned int number;
+    unsigned int numberOfStudents;
+    Student students[3];
+    
+    Potok() {}
+    
+    Potok(int number, Student students[]) {
+        this->number = number;
+        this->numberOfStudents = sizeof(this->students);
+        for(int i = 0; i<= this->numberOfStudents; i++) {
+            this->students[i] = students[i];
+        }
+    }
+    
+    friend std::ostream& operator<<(std::ostream& stream, Potok& potok);
+    
+    ~Potok() {
+        std::cout<<"Destructing <Potok>"<<std::endl;
+    }
+    
+    Student getStudentWithHighestGrade() {
+        Student currentStudent;
+        for(int i = 0; i <= this->numberOfStudents; i++) {
+            if(this->students[i].avScore > currentStudent.avScore) currentStudent = this->students[i];
+        }
+        return currentStudent;
+    }
+    
+    int writeStudentsIntoFile() {
+        if(sizeof(this->students) == 0) {
+            std::cout<<"Students array is empty."<<std::endl;
+            return 1;
+        }
+        
+        std::ofstream textFile;
+        textFile.open("students.txt");
+        
+        if(!textFile) {
+            std::cout<<"Cannot open output file."<<std::endl;
+            return 1;
+        }
+        
+        for(unsigned int i=0; i<=sizeof(this->students); i++) {
+            textFile<<this->students[i]<<std::endl;
+        }
+        
+        textFile.close();
+        
+        return 0;
+    }
+};
+
+std::ostream& operator<<(std::ostream& stream, Date& date) {
+    return stream<<date.day<<"/"<<date.month<<"/"<<date.year<<std::endl;
+}
+
+std::ostream& operator<<(std::ostream& stream, Student& student) {
+    return stream<<"Name: "<<student.name<<"Birth date: "<<student.birthDate<<"GPA: "<<student.avScore<<std::endl;
+}
+
+std::ostream& operator<<(std::ostream& stream, Potok& potok) {
+    for(int i = 0; i <= potok.numberOfStudents; i++) {
+        if(potok.students[i].showYears(potok.students[i].birthDate) >= 18 && potok.students[i].showYears(potok.students[i].birthDate) <= 26) {
+            stream<<potok.students[i].name;
+        }
+    }
+    return stream<<std::endl;
+}
+
+int main (int argc, const char * argv[]) {
+    Student *students[3];
+    students[0] = new Student("Teodor", "5109238", new Date(14, 8, 1995), 5.95);
+    students[1] = new Student("Julia", "5109237", new Date(17, 11, 1995), 5.12);
+    students[2] = new Student("Georgi", "5109236", new Date(5, 9, 1995), 4.49);
+    Potok *potok_1 = new Potok(1, *students);
+    potok_1->writeStudentsIntoFile();
+    
     return 0;
 }
